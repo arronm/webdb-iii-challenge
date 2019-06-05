63bdc0f01afef048e491d9b267c0150c4db61fd3
Arron M <marshall.arron@gmail.com>
2019-06-05 16:32:11 -0700

    Fix bug with the forEach not properly breaking and returning

--- middleware/validateBody.js
+++ middleware/validateBody.js
@@ -1,10 +1,16 @@
 // TODO: Update this further to have validation options like 'isRequired and type checking
 const validateBody = keys => (req, res, next) => {
+  let missing = '';
   keys.forEach(key => {
-    if (!req.body[key]) return res.status(400).json({
-      message: `Missing required field (${key})`,
-    });
+    if (!req.body[key]) {
+      missing = key;
+    }
   });
+
+  if (missing) return res.status(400).json({
+    message: `Missing required field (${missing})`,
+  });
+
   next();
 }
 
