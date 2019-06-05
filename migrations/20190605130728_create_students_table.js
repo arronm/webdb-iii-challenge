03ca0207d783068b8d8458f3bc11d670f7f1fe9d
Arron M <marshall.arron@gmail.com>
2019-06-05 13:59:03 -0700

    Create students table migration

--- migrations/20190605130728_create_students_table.js
+++ migrations/20190605130728_create_students_table.js
@@ -1,8 +1,19 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('students', students => {
+    students.increments();
+    students.string('name', 128)
+      .notNullable();
+    students.integer('cohort_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('cohorts')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('cohorts');
 };
