a24bc0bfb13d4211412027f540d2622d316e1fef
Arron M <marshall.arron@gmail.com>
2019-06-05 19:21:12 -0700

    Stretch project cleanup

--- index.js
+++ index.js
@@ -16,48 +16,6 @@ const middleware = [
 const server = express();
 server.use(middleware);
 
-// server.get('/', async (req, res) => {
-//   res.json({ message: 'API is working' });
-// });
-
-// server.get('/api', async (req, res) => {
-//   const data = await db.get();
-//   res.json(data);
-// });
-
-// server.get('/api/:id', async (req, res) => {
-//   const data = await db.get(req.params.id);
-//   res.json(data);
-// });
-
-// server.post('/api/test', async (req, res) => {
-//   try {
-//     const post = await db.add(req.body);
-//     res.status(201).json(post);
-//   } catch (error) {
-//     console.log(error.message);
-//     res.status(500).json(error);
-//   }
-// });
-
-// server.put('/api/test/:id', async (req, res) => {
-//   try {
-//     const put = await db.update(req.params.id, req.body);
-//     res.json(put);
-//   } catch (error) {
-//     res.status(500).json(error);
-//   }
-// });
-
-// server.delete('/api/test/:id', async (req, res) => {
-//   try {
-//     const record = await db.remove(req.params.id);
-//     res.json(record);
-//   } catch (error) {
-//     res.status(500).json(error);
-//   }
-// });
-
 server.use('/api/students', students);
 server.use('/api/cohorts', cohorts);
 
